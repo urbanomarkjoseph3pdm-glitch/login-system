e73f2ac05cd57309b7d606996771f6e14d9c037d
Mark Joseph Urbano <urbanomarkjoseph3pdm@gmail.com>
2026-09-19 00:01:37 +0800

    Update API URL to Render

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const API_URL = "http://127.0.0.1:5000";
+  const API_URL = "https://login-systemdb2.onrender.com";
 
   const loginForm = document.getElementById("loginForm");
   const registerForm = document.getElementById("registerForm");
